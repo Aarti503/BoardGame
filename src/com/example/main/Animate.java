2965794345eb5ed9a41bf884e45b4be8e954b4b5
Aarti Yadav <aartiy734@gmail.com>
2020-07-31 10:17:49 +0530

    First commit to game

---
+++ src/com/example/main/Animate.java
@@ -0,0 +1,25 @@
+package com.example.main;
+
+public class Animate implements Runnable{
+
+	BlockBreakerPanel bp;
+	
+	Animate(BlockBreakerPanel b){
+		bp = b;
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			bp.update();
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
